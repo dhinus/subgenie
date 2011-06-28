52b27bed724ebdf3ac8d09635fb417d3032e33f0
Francesco Negri <francesco.negri@cayenne.it>
2011-06-28 10:27:37 +0200

    v0.1

---
+++ install.sh
@@ -0,0 +1,9 @@
+echo
+echo "Press enter to install mergeninja to /usr/local/bin"
+echo "(Your password will be required)"
+echo
+echo "Press CTRL+C to cancel"
+echo
+read
+sudo cp mergeninja /usr/local/bin
+sudo chmod 755 /usr/local/bin/mergeninja
