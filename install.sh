3f3324c38ef5f545137fe7daac2ee5345e48250b
Francesco Negri <francesco.negri@cayenne.it>
2012-05-07 11:55:37 +0200

    removed leftover ninja references

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 echo
-echo "This will install mergeninja to /usr/local/bin"
+echo "This will install subgenie to /usr/local/bin/sgn"
 echo "(Your password may be required)"
 echo
 echo "Press Enter to continue or CTRL+C to cancel"
 read
-sudo cp mergeninja /usr/local/bin
-sudo chmod 755 /usr/local/bin/mergeninja
+sudo cp sgn /usr/local/bin
+sudo chmod 755 /usr/local/bin/sgn
 echo "Sweet."
 echo
