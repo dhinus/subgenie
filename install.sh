1ef200be469218a2fe0e49d07a1451c5940b4e9b
Francesco Negri <francesco.negri@cayenne.it>
2012-05-07 11:16:04 +0200

    politeness is everything

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 echo
-echo "Press enter to install mergeninja to /usr/local/bin"
-echo "(Your password will be required)"
+echo "This will install mergeninja to /usr/local/bin"
+echo "(Your password may be required)"
 echo
-echo "Press CTRL+C to cancel"
-echo
+echo "Press Enter to continue or CTRL+C to cancel"
 read
 sudo cp mergeninja /usr/local/bin
 sudo chmod 755 /usr/local/bin/mergeninja
+echo "Sweet."
+echo
